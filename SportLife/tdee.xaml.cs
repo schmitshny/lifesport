a8a597678587c1d1c6b707ca0368f30658d2142a
Marcin Bugaj <mbugaj93@gmail.com>
2021-02-18 09:53:48 +0100

    database for tdee data

--- SportLife/tdee.xaml.cs
+++ SportLife/tdee.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,9 +24,42 @@ namespace SportLife
         public tdee()
         {
             InitializeComponent();
+            databaseEntities db = new databaseEntities();
+
+            var query = (from x in db.MEASURES
+                         where x.Id == 1
+                         select x).FirstOrDefault();
+            
+            if(query!=null)
+             { 
+                weight.Text = query.weight.ToString();
+                age.Text = query.age.ToString();
+                height.Text = query.height.ToString();
+                activityfactor.SelectedIndex = (int)query.activity;
+                weightChange.SelectedIndex = (int)query.weightchange;
+             }
+
+            var query_2 = (from x in db.macrosandtdee
+                           where x.Id == 1
+                           select x).FirstOrDefault();
+
+            tdee1.Text = query_2.wtdee.ToString();
+            deficyt.Text = query_2.deficyt.ToString();
+            needeat.Text = query_2.needeat.ToString();
+            protein.Text = query_2.protein.ToString();
+            fat.Text = query_2.fat.ToString();
+            carbs.Text = query_2.carbs.ToString();
+
         }
+
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+
+
+
             if (this.weight.Text == "" || this.age.Text == "" || this.height.Text == "" || this.weightChange.Text == "" || this.activityfactor.Text == "" || this.weightChange.Text == "")
             {
                 MessageBox.Show("Please enter all data");
@@ -35,9 +69,9 @@ namespace SportLife
                 this.tdee1.Text = "dsfsdffs";
 
 
-                double weight = int.Parse(this.weight.Text);
-                double age = int.Parse(this.age.Text);
-                double height = int.Parse(this.height.Text);
+                int weight = int.Parse(this.weight.Text);
+                int age = int.Parse(this.age.Text);
+                int height = int.Parse(this.height.Text);
                 double activity;
 
                 switch (activityfactor.SelectedIndex)
@@ -59,8 +93,8 @@ namespace SportLife
                         break;
                 }
 
-                double tdee = ((9.99 * weight) + (6.25 * height) - (4.92 * age) + 5) * activity;
-                this.tdee1.Text = tdee.ToString();
+                double tdeee = ((9.99 * weight) + (6.25 * height) - (4.92 * age) + 5) * activity;
+                this.tdee1.Text = tdeee.ToString();
 
                 double def;
 
@@ -94,12 +128,70 @@ namespace SportLife
 
                 this.deficyt.Text = (def * 1100).ToString();
 
-                this.needeat.Text = (tdee + def * 1100).ToString();
+                this.needeat.Text = (tdeee + def * 1100).ToString();
 
-                this.protein.Text = ((tdee + def * 0.2) / 4).ToString("F0");
-                this.fat.Text = ((tdee + def * 0.25) / 9).ToString("F0");
-                this.carbs.Text = ((tdee + def * 0.55) / 4).ToString("F0");
+                this.protein.Text = ((tdeee + def * 0.2) / 4).ToString("F0");
+                this.fat.Text = ((tdeee + def * 0.25) / 9).ToString("F0");
+                this.carbs.Text = ((tdeee + def * 0.55) / 4).ToString("F0");
+
+
+                databaseEntities db = new databaseEntities();
+
+                var query = (from x in db.MEASURES
+                             where x.Id == 1
+                             select x);
+
+                MEASURES obj = query.SingleOrDefault();
+                obj.weight = int.Parse(this.weight.Text);
+                obj.age = int.Parse(this.age.Text);
+                obj.height = int.Parse(this.height.Text);
+                obj.activity = activityfactor.SelectedIndex;
+                obj.weightchange = weightChange.SelectedIndex;
+
+                /////
+                ///
+                var query_2 = (from x in db.macrosandtdee
+                               where x.Id == 1
+                               select x);
+
+                macrosandtdee o = query_2.SingleOrDefault();
+                o.wtdee = (int)tdeee ;
+                o.deficyt = (int)(def * 1100);
+                o.needeat = (int)(tdeee + def * 1100);
+                o.protein = (int)((tdeee + def * 0.2) / 4);
+                o.fat = (int)((tdeee + def * 0.25) / 9);
+                o.carbs = (int)((tdeee + def * 0.55) / 4);
+
+
+                db.SaveChanges();
+
+
+
+
             }
+
+            
+
+            
+
         }
+
+        private void weight_previewtextinput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+           
+        }
+
+        private void age_previewtextinput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void height_previewtextinput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+
     }
 }
